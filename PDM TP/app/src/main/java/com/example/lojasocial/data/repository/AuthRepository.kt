579bf9d25bbdcf32f93d8bfe0df7428f48557bf4
emanuelbarroscunha <74781201+emanuelbarroscunha@users.noreply.github.com>
2025-01-04 17:45:52 +0000

    Alterações Login e Registo

---
+++ PDM TP/app/src/main/java/com/example/lojasocial/data/repository/AuthRepository.kt
@@ -0,0 +1,46 @@
+package com.example.lojasocial.data.repository
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+object AuthRepository {
+    private val auth by lazy { FirebaseAuth.getInstance() }
+    private val firestore by lazy { FirebaseFirestore.getInstance() }
+
+    fun login(
+        email: String,
+        password: String,
+        onSuccess: (String) -> Unit,
+        onFailure: (String) -> Unit
+    ) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val userId = auth.currentUser?.uid
+                    if (userId != null) {
+                        firestore.collection("user").document(userId).get()
+                            .addOnSuccessListener { document ->
+                                if (document.exists()) {
+                                    val role = document.getString("role") ?: "default"
+                                    onSuccess(role) // Passa o role para o sucesso
+                                } else {
+                                    onFailure("Dados do utilizador não encontrados.")
+                                }
+                            }
+                            .addOnFailureListener { e ->
+                                onFailure("Erro ao buscar dados: ${e.message}")
+                            }
+                    } else {
+                        onFailure("Erro ao obter ID do usuário.")
+                    }
+                } else {
+                    onFailure(task.exception?.localizedMessage ?: "Erro ao fazer login.")
+                }
+            }
+    }
+
+    fun logout(onSuccess: () -> Unit) {
+        auth.signOut()
+        onSuccess()
+    }
+}
